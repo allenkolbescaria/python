8cd3c2613c28805c54d3a79d08bf834f16459a82
allenkolbescaria <allenkolbescaria.5b@gmail.com>
2026-07-17 10:47:51 +0530

    lesson 2 completed

---
+++ lesson2/activity2.py
@@ -0,0 +1,9 @@
+a=20
+b=10
+c="30"
+d="50"
+name=input("enter your name")
+print(a+b)
+print(c+d)
+print(name)
+print(name+c)
---
+++ lesson2/hello.py
@@ -0,0 +1,10 @@
+print("hello to the world of python program")
+print("4")
+print(4)
+a=4
+print(f"the value of a is {a}")
+print("the value of a is",a)
+b=9
+print('a+b=',a+b)
+
+
