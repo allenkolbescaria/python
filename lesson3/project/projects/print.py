eb94a89b6b04275b72f6f7d379b8293ba3c7328e
allenkolbescaria <allenkolbescaria.5b@gmail.com>
2026-07-22 11:51:36 +0530

    project1

---
+++ lesson3/project/projects/print.py
@@ -0,0 +1,6 @@
+a=input("enter your name")
+b=input(f"hello what are your hobbies {a}")
+c=int(input(f"how many hours do you spent on {b}"))
+
+d= int(input(f"how many hours do you study?"))
+print(f"so you spend {c} hours on {b} and {d} hours on studying")
---
+++ lesson3/project/projects/variables.py
@@ -0,0 +1,4 @@
+a=input("enter your date of birth")
+print(a)
+b="07/07/2007"
+print(b)
